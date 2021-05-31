02c8d7049c106519c32bf08e29d06d1770c9eaf6
bulbulica2 <necrocris2000@yahoo.com>
2021-05-31 23:54:30 +0300

    Added vuetify to main.js and tested the vue components

--- src/main.js
+++ src/main.js
@@ -1,12 +1,31 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
+import isElectron from 'is-electron';
 import App from './App.vue';
 import router from './router';
+
 import store from './store';
+
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+const vuetifyOptions = {};
+
+// check for desktop or web app
+if (isElectron()) {
+  new Vue({
+    router,
+    vuetify: new Vuetify(vuetifyOptions),
+    render: (h) => h(App),
+  }).$mount('#app');
+} else {
+  new Vue({
+    router,
+    store,
+    vuetify: new Vuetify(vuetifyOptions),
+    render: (h) => h(App),
+  }).$mount('#app');
+}
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-app>
+    <div class="about">
+      <h1>This is an about page</h1>
+      <v-icon>mdi-account</v-icon>
+      <span class="group pa-2">
+        <v-icon>home</v-icon>
+        <v-icon>event</v-icon>
+        <v-icon>info</v-icon>
+      </span>
+      <v-row
+        align="center"
+        class="mx-0"
+      >
+        <v-rating
+          :value="4.5"
+          color="amber"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
+        <div class="grey--text ms-4">
+          4.5 (413)
+        </div>
+      </v-row>
+      <v-btn
+        elevation="2"
+      >Click me
+      </v-btn>
+    </div>
+  </v-app>
 </template>
